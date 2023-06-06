15722c1af3d927f26af70b0e0db89f5a9bfbbcd9
WorldlineChanger <worldlinechanger@163.com>
2023-06-06 12:30:09 +0100

    Upload

---
+++ DinnerDanmakuBlock.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name        屏蔽B站大会员彩色弹幕
+// @description 屏蔽大会员发送的专属彩色弹幕
+// @author      WorldlineChanger
+// @namespace   https://github.com/WorldlineChanger/DinnerDanmakuBlockScript
+// @homepageURL    https://github.com/WorldlineChanger/DinnerDanmakuBlockScript
+// @license     MIT
+// @match       *://www.bilibili.com/video/av*
+// @match       *://www.bilibili.com/video/BV*
+// @match       *://www.bilibili.com/bangumi/play/ep*
+// @match       *://www.bilibili.com/bangumi/play/ss*
+// @match       *://m.bilibili.com/bangumi/play/ep*
+// @match       *://m.bilibili.com/bangumi/play/ss*
+// @match       *://bangumi.bilibili.com/anime/*
+// @match       *://bangumi.bilibili.com/movie/*
+// @match       *://www.bilibili.com/bangumi/media/md*
+// @match       *://www.bilibili.com/blackboard/html5player.html*
+// @match       *://www.bilibili.com/watchroom/*
+// @match       *://space.bilibili.com/*
+// @match       https://www.bilibili.com/
+// @match       https://www.bilibili.com/?*
+// @grant       none
+// @icon        https://www.bilibili.com/favicon.ico
+// @version     1.0
+
+// ==/UserScript==
+
+(function () {
+ 'use strict';
+ var StyleElement = document.createElement('style');
+ StyleElement.innerText = '.bili-dm-vip{display:none !important; }';
+ document.body.appendChild(StyleElement);
+})();
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # DinnerDanmakuBlockScript
-屏蔽B站大会员彩色弹幕
+**屏蔽大会员发送的专属彩色弹幕**
+
+不包含普通彩色弹幕
+
+![Dinner](https://cdn.staticaly.com/gh/WorldlineChanger/Air-Image-Cloud@main/20230604/4bc79f26cffc1e174f216afc0f90f603718de9d4.3k4k9hkzl160.webp)
